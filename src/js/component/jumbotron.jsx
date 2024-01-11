9ae150cb9977a1655b4e01ca3b02b7e563f29899
Dana M <147849932+dkfmedia@users.noreply.github.com>
2024-01-11 02:12:18 +0000

    Jumbotron finalized

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -1,17 +1,24 @@
 import React from "react";
-
+import PropTypes from 'prop-types';
 
 // change all this for the jumbotron
 const Jumbotron = (props) => {
     return (
-    <div className="jumbotron m-5"> 
+    <div className="jumbotron m-5 bg-dark text-light rounded p-4"> 
     <h1 className="display-4">{props.title}</h1>
     <p className= "lead"> {props.description}</p>
-    <a className= "btn btn-primary btn-lg" href={props.buttonURL} role="button">
+    <a className= "btn btn-secondary btn-lg" href={props.buttonURL} role="button">
                 {props.buttonLabel}
         </a>
 </div>
 )}; 
+
+Jumbotron.propTypes = { 
+        title: PropTypes.string,
+        description: PropTypes.string, 
+        buttonURL: PropTypes.string, 
+        buttonLabel: PropTypes.string,
+}
 
 
 export default Jumbotron;
