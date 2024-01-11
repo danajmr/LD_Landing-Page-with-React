6bb5ae39f3f615401c23b84e441e76e6cbb42603
Dana M <147849932+dkfmedia@users.noreply.github.com>
2024-01-11 00:44:42 +0000

    Jumbotron up and runninggit add .git add .git add .!

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,20 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// import rigoImage from "../../img/rigo-baby.jpg";
 
 // change all this for the jumbotron
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+const Jumbotron = (props) => {
+    return (
+    <div className="jumbotron m-5"> 
+    <h1 className="display-4">{props.title}</h1>
+    <p className= "lead"> {props.description}</p>
+    <a className= "btn btn-primary btn-lg" href={props.buttonURL} role="button">
+                {props.buttonLabel}
+        </a>
+</div>
+)}; 
 
-export default Home;
+
+
+export default Jumbotron;
