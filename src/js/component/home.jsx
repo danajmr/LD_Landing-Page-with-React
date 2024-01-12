3078eea751db59e2feb14509c0dbf1bef06b37f4
Taylor Allen <50384820+taylor-allen@users.noreply.github.com>
2024-01-12 17:49:56 +0000

    final review

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -11,9 +11,9 @@ const Home = () => {
 			<Header />
 			<Jumbotron 
 				title ="Welcome to this nice page designed by 3 amazing devs"
-				description= "Pellentesque non erat ac nisl pharetra fermentum ac et quam. Quisque vitae odio eu orci eleifend placerat eu id tortor. Proin nec est eget velit suscipit euismod. Integer non ultrices lacus. Ut ullamcorper convallis ultrices. Pellentesque dignissim malesuada ex sit amet ornare. Cras purus est, tincidunt id feugiat sed, ullamcorper eu est. Nullam vulputate odio mi, vitae imperdiet justo euismod eget. Pellentesque mollis pellentesque purus sit amet tristique.
+				description= {'Pellentesque non erat ac nisl pharetra fermentum ac et quam. Quisque vitae odio eu orci eleifend placerat eu id tortor. Proin nec est eget velit suscipit euismod. Integer non ultrices lacus. Ut ullamcorper convallis ultrices. Pellentesque dignissim malesuada ex sit amet ornare. Cras purus est, tincidunt id feugiat sed, ullamcorper eu est. Nullam vulputate odio mi, vitae imperdiet justo euismod eget. Pellentesque mollis pellentesque purus sit amet tristique.
 				Neque porro quisquam est qui dolorem ipsum quia dolor sit amet consectetur, adipisci velit...
-				There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain.."
+				There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..}
 				buttonLabel="Click here if you want a free JavaScript ebook"
 				buttonURL="https://eloquentjavascript.net/"
 				/>
